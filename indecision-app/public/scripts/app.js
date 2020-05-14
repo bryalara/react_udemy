199f2b4e53ade448dade3046d6f999b645c279ec
Bryan <bryalara@tamu.edu>
2020-05-14 02:35:00 -0500

    .

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -3,9 +3,9 @@
 console.log("App.js is running");
 
 var template = React.createElement(
-  "p",
+  "h1",
   null,
-  "This is JSX from app.js!"
+  "Indecision App"
 );
 
 var appRoot = document.getElementById("app");
